85749e129209a2d12c553687b625ec36be4d6ccd
james <jtwebco@gmail.com>
2019-08-18 21:58:31 -0400

    fixes reddit function

--- cmd/red.js
+++ cmd/red.js
@@ -1,7 +1,9 @@
 const fetch = require('node-fetch')
 
+let instance = 0
+
 exports.fetchRed = async (msg, richEmbed) => {
-  
+
   let repeat = true;
 
   const index = [`https://www.reddit.com/r/educationalgifs.json?sort=hot&t=all`,
@@ -12,39 +14,32 @@ exports.fetchRed = async (msg, richEmbed) => {
 
   let url = Math.floor((Math.random() * index.length));
 
-    while (repeat) {
-    try {let data = await fetch(index[url]),
-             json = await data.json()
-    if (json) {let rand = Math.ceil(Math.random() * (json.data.children.length - 1) + 1),
+  while (repeat) {
+    instance++
+    console.log('running reddit: ', instance)
+    try {
+      let data = await fetch(index[url]),
+              json = await data.json()
+      if (json) {
+        let rand = Math.ceil(Math.random() * (json.data.children.length - 1) + 1),
                    post = json.data.children[rand].data
-                   post.url
-  
-          if (post.url.startsWith('https://gfy')) post.url += '.gif'
-          if (post.url.startsWith('http://gfy')) post.url += '.gif'
-          if (post.url.startsWith('gfy'))  post.url += '.gif'
-          if (post.url.startsWith('https://i.imgur')) post.url += '.gifv'
-          
-      
-       imgPush = [post.title, post.url]
-   
-    repeat=false; 
 
+        if (post.url.startsWith('https://gfy')) post.url += '.gif'
+        if (post.url.startsWith('http://gfy')) post.url += '.gif'
+        if (post.url.startsWith('gfy'))  post.url += '.gif'
+        if (post.url.startsWith('https://i.imgur')) post.url += '.gifv'
 
+        imgPush = [post.title, post.url]
+
+        if (post.url.startsWith('https://v.')) {
+          setTimeout(() => { 0repeat = true }, 500)
+        } else {
+          repeat = false
+          msg.channel.send(imgPush)
+        }
+      }
+    } catch (e) {
+      console.error('reddit error: ', e)
+    }
   }
-  
-}
-catch (ERROR) {
-  if (post.url.startsWith('https://v.'))
-  repeat=true;
-
-  console.error('red error: ', ERROR)
-
-}
-
-msg.channel.send(imgPush)
-
-}
-
- 
-
 }
