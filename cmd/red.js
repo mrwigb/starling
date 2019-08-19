d3bff17a2cd93c1c19935efcbb0a55ffc26b035b
james <jtwebco@gmail.com>
2019-08-18 22:00:53 -0400

    typo

--- cmd/red.js
+++ cmd/red.js
@@ -6,19 +6,19 @@ exports.fetchRed = async (msg, richEmbed) => {
 
   let repeat = true;
 
-  const index = [`https://www.reddit.com/r/educationalgifs.json?sort=hot&t=all`,
+  const urls = [`https://www.reddit.com/r/educationalgifs.json?sort=hot&t=all`,
                 `https://www.reddit.com/r/oddlysatisfying.json?sort=hot&t=all`,
                 `https://www.reddit.com/r/mesmerizinggifs.json?sort=hotl&t=all`,
                 `https://www.reddit.com/r/pic/.json?sort=hot&t=all`,
                 `https://www.reddit.com/r/pic/.json?sort=controversial&t=all`]
 
-  let url = Math.floor((Math.random() * index.length));
+  let index = Math.floor((Math.random() * urls.length));
 
   while (repeat) {
     instance++
     console.log('running reddit: ', instance)
     try {
-      let data = await fetch(index[url]),
+      let data = await fetch(urls[index]),
               json = await data.json()
       if (json) {
         let rand = Math.ceil(Math.random() * (json.data.children.length - 1) + 1),
@@ -32,7 +32,7 @@ exports.fetchRed = async (msg, richEmbed) => {
         imgPush = [post.title, post.url]
 
         if (post.url.startsWith('https://v.')) {
-          setTimeout(() => { 0repeat = true }, 500)
+          setTimeout(() => { repeat = true }, 500)
         } else {
           repeat = false
           msg.channel.send(imgPush)
